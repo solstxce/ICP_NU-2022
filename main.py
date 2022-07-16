8f22d2bb743ff9af1a445b9b87f26542b6a2a27f
solstxce <48374053+solstxce@users.noreply.github.com>
2022-07-16 17:30:14 +0530

    Edited main.py
    edited main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from functions import *
 import os
 import time
 def logo():
+  os.system("clear")
   print("""
   ████████ ██                      ██        
  ██░░░░░░ ░░              ██████  ░██        
@@ -27,7 +28,7 @@ def info():
     [2] Binary
     [3] Hex
     [4] Oct
-    [5] Select this to exit the program.
+    [q] Select this to exit the program.
      """)
   choice = input("[?]: ")
   return choice
@@ -37,36 +38,36 @@ def repeatConv(Ans):
    os.system('clear')
    choiceVar=info()
    main(choiceVar)
-  elif strAns=='n' or strAns=='no':
+  elif strAns=='n' or strAns=='no' or strAns=='q':
     main('5')
   else:
     print('Please choose any appropriate answer. (Either y or n)')
-    repeatConv(input('Do you wanna continue? \n Answer in y(yes) or n(no): '))
+    repeatConv(input('Do you wanna continue? \nAnswer y(yes) or n(no) or q(quit): '))
 def main(choice):
  if choice == '1':
    getInt=int(input("Enter a valid number: "))
    print('The number in binary format:',int_to_bin(getInt))
    print('The number in hex format: ',int_to_hex(getInt))
    print('The number in oct format: ', int_to_oct(getInt))
-   repeatConv(input('\nDo you wanna continue? \nAnswer y(yes) or n(no): '))
+   repeatConv(input('\nDo you wanna continue? \nAnswer y(yes) or n(no) or q(quit): '))
  elif choice =='2':
    getBin=input("Enter a valid binary string: ")
    print('The binary string as integer:',bin_to_int(getBin))
    print('The binary string in hex:',bin_to_hex(getBin))
    print('The binary string in oct:',bin_to_oct(getBin))
-   repeatConv(input('\nDo you wanna continue? \nAnswer y(yes) or n(no): '))
+   repeatConv(input('\nDo you wanna continue? \nAnswer y(yes) or n(no) or q(quit): '))
  elif choice=='3':
    getHex=input("Enter a valid hex string: ")
    print("The hex string as integer:",hex_to_int(getHex))
    print("The hex string in binary:",hex_to_bin(getHex))
    print("The hex string in oct:",hex_to_oct(getHex))
-   repeatConv(input('\nDo you wanna continue? \nAnswer y(yes) or n(no): '))
+   repeatConv(input('\nDo you wanna continue? \nAnswer y(yes) or n(no) or q(quit): '))
  elif choice =='4':
     getOct=input('Enter a valid oct string: ')
     print("The hex string as integer:",oct_to_int(getOct))
     print("The hex string in binary:",oct_to_bin(getOct))
     print("The hex string in hex:",oct_to_hex(getOct))
-    repeatConv(input('\nDo you wanna continue? \nAnswer y(yes) or n(no): '))
+    repeatConv(input('\nDo you wanna continue? \nAnswer y(yes) or n(no) or q(quit): '))
  elif choice=='5' or choice=='q':
    print("Thank you for using the string converter!")
    os.system('exit')
